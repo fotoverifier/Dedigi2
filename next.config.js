dc39deb4a28fbc9c668b5e70c1823a287017a4a8
Duy-Nam Ly <duynamvt79@gmail.com>
2023-09-07 14:13:32 +0700

    backup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-output: 'standalone',
+    transpilePackages: ['@acme/ui', 'lodash-es'],
+    output: 'standalone',
   images: {
     remotePatterns: [
         {
