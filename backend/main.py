60cefea1e99352ffbd322f3c660631c01a167188
fotoverifier <fotoverifier@gmail.com>
2023-09-26 00:59:35 +0700

    update backend to listen on all interface

--- backend/main.py
+++ backend/main.py
@@ -101,4 +101,4 @@ async def process_only(file_name: Annotated[str, Form()], quality: Annotated[int
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
